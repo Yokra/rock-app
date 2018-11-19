ecf79540d32c2149433be2e98196cf64ef00eae3
yokra <yordanka_krasteva@mail.bg>
2018-11-19 11:51:45 +0100

    save

---
+++ details.js
@@ -0,0 +1,32 @@
+const template = document.querySelector("#rockTemplate").content;
+
+//1. fetch content
+function getRock() {
+    fetch("https://martanetza.com/kea/cms/huset_kbh/wp-json/wp/v2/music?categories=1&_embed")
+        .then(res => res.json())
+        .then(showRock)
+}
+
+function showRock(rockList) {
+    console.log(rockList)
+    rockList.forEach(showSingleRock)
+}
+
+function showSingleRock(rock) {
+    console.log(rock)
+    //, make a copy of the template
+    const copy = template.cloneNode(true);
+    copy.querySelector(".groupname").textContent = rock.title.rendered;
+    copy.querySelector(".day").textContent = rock.acf.day;
+    copy.querySelector(".date").textContent = rock.acf.date;
+    copy.querySelector(".venue").textContent = rock.acf.venue;
+    copy.querySelector(".onlineprice span").textContent = rock.acf.online_price;
+    copy.querySelector(".doorhour span").textContent = rock.acf.door_hour;
+    copy.querySelector(".hour span").textContent = rock.acf.hour;
+    copy.querySelector(".artistdescription").textContent = rock.acf.artist_description;
+    copy.querySelector("img").src =rock.acf.artist_photos.sizes.medium;
+
+    document.querySelector("main").appendChild(copy)
+}
+
+getRock();
---
+++ index.js
@@ -0,0 +1,28 @@
+const template = document.querySelector("#rockTemplate").content;
+
+//1. fetch content
+function getRock() {
+    fetch("https://martanetza.com/kea/cms/huset_kbh/wp-json/wp/v2/music?categories=1&_embed")
+        .then(res => res.json())
+        .then(showRock)
+}
+
+function showRock(rockList) {
+    console.log(rockList)
+    rockList.forEach(showSingleRock)
+}
+
+function showSingleRock(rock) {
+    console.log(rock)
+    //, make a copy of the template
+    const copy = template.cloneNode(true);
+    copy.querySelector(".groupname").textContent = rock.title.rendered;
+    copy.querySelector(".day").textContent = rock.acf.day;
+    copy.querySelector(".date").textContent = rock.acf.date;
+
+    copy.querySelector("img").src = rock._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+
+    document.querySelector("main").appendChild(copy)
+}
+
+getRock();
