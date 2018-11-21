258b9ddc442b2ddea7a8fbe95e38960e78fe34b2
yokra <yordanka_krasteva@mail.bg>
2018-11-21 12:18:02 +0100

    grid and styling for contact page

--- contact.js
+++ contact.js
@@ -16,16 +16,15 @@ function showSingleContacts(contacts) {
     console.log(contacts)
     //, make a copy of the template
     const copy = template.cloneNode(true);
-    copy.querySelector("h1").textContent = contacts.title.rendered;
+    //copy.querySelector(".contact").textContent = contacts.title.rendered;
     copy.querySelector(".placename").textContent = contacts.acf.place_name;
     copy.querySelector(".address").textContent = contacts.acf.address;
     copy.querySelector(".postalcode").textContent = contacts.acf.postal_code;
     copy.querySelector(".phone").textContent = contacts.acf.phone;
     copy.querySelector(".mainemail").textContent = contacts.acf.main_email;
     copy.querySelector(".info").textContent = contacts.acf.info;
-    copy.querySelector(".info2").textContent = contacts.acf.info2;
 
-    copy.querySelector("img").src = contacts._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
+    copy.querySelector(".contactimage").src = contacts._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
 
     document.querySelector("main").appendChild(copy)
 }
